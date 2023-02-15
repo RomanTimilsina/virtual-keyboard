4fe70b0aa9527e7e0fd473c175535c99b6f32348
Roman <romantimilsina@gmail.com>
2023-02-15 12:31:03 +0545

    created_keys

--- keyboard/Keyboard.js
+++ keyboard/Keyboard.js
@@ -25,10 +25,120 @@ const Keyboard = {
     this.elements.keysContainer.appendChild(this._createKeys());
     this.elements.main.appendChild(this.elements.keysContainer)
     document.body.appendChild(this.elements.main)
+
+    document.querySelector('.use-keyboard-input').addEventListener('focus', () => {
+      
+    })
   },
 
   _createKeys(){
     const fragment = document.createDocumentFragment();
+    const keyLayout = [
+      "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
+      "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+      "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
+      "done", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
+      "space"
+  ];
+
+  const createIcon = (icon) => {
+    return `<i class="material-icons">${icon}</i>`
+  }
+
+  keyLayout.forEach(key => {
+    const keyElement = document.createElement('button');
+    const insertLineBreak = ["backspace", "p", "enter", "?"].indexOf(key) !== -1;
+    
+    keyElement.setAttribute('type','button')
+    keyElement.classList.add('keyboard__key')
+
+    switch(key){
+      case "backspace" : 
+      keyElement.classList.add('keyboard__key--wide');
+      keyElement.innerHTML = createIcon('backspace');
+
+      keyElement.addEventListener('click', (e) => {
+        this.properties.value = this.properties.value.substring(0, this.properties.value.length - 1);
+        console.log(this.properties.value)
+      })
+
+      break;
+
+      case "space" : 
+      keyElement.classList.add('keyboard__key--extra-wide');
+      keyElement.innerHTML = createIcon('space_bar');
+
+      keyElement.addEventListener('click', (e) => {
+        this.properties.value += ' ';
+        console.log(this.properties.value + 'a')
+      })
+
+      break;
+
+      case "space" : 
+      keyElement.classList.add('keyboard__key--extra-wide');
+      keyElement.innerHTML = createIcon('space_bar');
+
+      keyElement.addEventListener('click', (e) => {
+        this.properties.value += ' ';
+        console.log(this.properties.value + 'a')
+      })
+
+      break;
+
+      case "caps" : 
+      keyElement.classList.add('keyboard__key--wide','keyboard__key--activatable');
+      keyElement.innerHTML = createIcon('keyboard_capslock');
+
+      keyElement.addEventListener('click', (e) => {
+        this._toggleCapsLock();
+        keyElement.classList.toggle('keyboard__key--active', this.properties.capslock);
+      })
+
+      break;
+
+      case "enter" : 
+      keyElement.classList.add('keyboard__key--wide');
+      keyElement.innerHTML = createIcon('keyboard_return');
+
+      keyElement.addEventListener('click', (e) => {
+        this.properties.value += '\n';
+        console.log(this.properties.value + 'a')
+      })
+
+      break;
+
+      case "done" : 
+      keyElement.classList.add('keyboard__key--wide','keyboard__key--dark');
+      keyElement.innerHTML = createIcon('check_circle');
+
+      keyElement.addEventListener('click', (e) => {
+        this.elements.main.classList.add('keyboard__hidden')
+      })
+
+      break;
+
+      default:
+      keyElement.innerHTML = key;
+
+      keyElement.addEventListener('click', () => {
+        this.properties.value += this.properties.capslock ? key.toUpperCase() : key.toLowerCase()
+        console.log(this.properties.value )
+      })
+
+      break;
+
+      
+    }
+    fragment.appendChild(keyElement);
+
+    if(insertLineBreak) {
+      fragment.appendChild(document.createElement('br'))
+    }
+    
+  })
+
+  return fragment;
   },
 
   _triggerEvent(handlerName){
@@ -36,7 +146,7 @@ const Keyboard = {
   },
 
   _toggleCapsLock(){
-
+    this.properties.capslock = !this.properties.capslock
   },
 
   open(initialValue, oninput, onclose){
